ff13136432db616f9498acbb4dbc3877cd1f39fd
Zeke Zhang <sean1832725142@gmail.com>
2024-07-25 04:55:52 +1000

    feat(compression): Add gzip compression and decompression methods

---
+++ Portal.Core/Compression/GZip.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Portal.Core.Compression
+{
+    public class GZip
+    {
+        public static byte[] Compress(byte[] byteArray)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+            {
+                gzipStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            return memoryStream.ToArray();
+        }
+
+        public static byte[] Decompress(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+                return null;
+
+            using var memoryStream = new MemoryStream(data);
+            using var gzipStream = new GZipStream(memoryStream, CompressionMode.Decompress);
+            using var resultStream = new MemoryStream();
+
+            gzipStream.CopyTo(resultStream);
+            return resultStream.ToArray();
+        }
+    }
+}
