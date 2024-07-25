918b491be728137bc5b5c9069d632d8e0d12c928
Zeke Zhang <sean1832725142@gmail.com>
2024-07-25 22:43:26 +1000

    feat(io): add file writer and reader component

---
+++ Portal.Gh/Components/Local/FileReaderComponent.cs
@@ -0,0 +1,56 @@
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Portal.Gh.Common;
+using Portal.Gh.Params.Bytes;
+
+namespace Portal.Gh.Components.Local
+{
+    public class FileReaderComponent : GH_Component
+    {
+        #region Metadata
+
+        public FileReaderComponent()
+            : base("File Reader", "<File>",
+                "Read a file in bytes",
+                Config.Category, Config.SubCat.LocalIpc)
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+        public override IEnumerable<string> Keywords => new string[] { };
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("94cca7d1-589e-4a52-ab6d-82b6325cad67");
+
+        #endregion
+
+        #region IO
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("File Path", "path", "file path to read", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddParameter(new BytesParam(), "Bytes", "Bytes", "bytes array read", GH_ParamAccess.item);
+        }
+
+        #endregion
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string path = string.Empty;
+
+            if (!DA.GetData(0, ref path)) return;
+
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            BytesGoo bytesGoo = new BytesGoo(bytes);
+
+            DA.SetData(0, bytesGoo);
+        }
+    }
+}
---
+++ Portal.Gh/Components/Local/FileWriterComponent.cs
@@ -0,0 +1,62 @@
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Portal.Gh.Common;
+using Portal.Gh.Params.Bytes;
+
+namespace Portal.Gh.Components.Local
+{
+    public class FileWriterComponent : GH_Component
+    {
+        #region Metadata
+
+        public FileWriterComponent()
+            : base("File Writer", ">File<",
+                "Write a file in bytes",
+                Config.Category, Config.SubCat.LocalIpc)
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+        public override IEnumerable<string> Keywords => new string[] { };
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("cdbb0c22-a0b5-43bd-a937-7bf7549c45e7");
+
+        #endregion
+
+        #region IO
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new BytesParam(), "Bytes", "Bytes", "bytes to write", GH_ParamAccess.item);
+            pManager.AddTextParameter("File Path", "path", "file path to write", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Write", "write", "start writing", GH_ParamAccess.item, false);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+        }
+
+        #endregion
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            byte[] data = null;
+            string path = string.Empty;
+            bool write = false;
+
+            if (!DA.GetData(0, ref data)) return;
+            if (!DA.GetData(1, ref path)) return;
+            if (!DA.GetData(2, ref write)) return;
+
+            if (write)
+            {
+                System.IO.File.WriteAllBytes(path, data);
+                Message = "Written";
+            }
+        }
+    }
+}
