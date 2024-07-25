1254cef93ea25844cf359cf8077bbe10cc0bb7ff
Zeke Zhang <sean1832725142@gmail.com>
2024-07-25 22:43:50 +1000

    feat(utils): add bytes decoder and encoder

---
+++ Portal.Gh/Components/Utils/BytesDecoderComponent.cs
@@ -0,0 +1,55 @@
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Portal.Gh.Common;
+using Portal.Gh.Params.Bytes;
+
+namespace Portal.Gh.Components.Utils
+{
+    public class BytesDecoderComponent : GH_Component
+    {
+        #region Metadata
+
+        public BytesDecoderComponent()
+            : base("Bytes Decoder", "DeBytes",
+                "Decode a byte array into text",
+                Config.Category, Config.SubCat.Utils)
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+        public override IEnumerable<string> Keywords => new string[] { };
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("1cb92168-973b-4d72-b4d0-907eb6c5bd61");
+
+        #endregion
+
+        #region IO
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new BytesParam(), "Bytes", "Bytes", "Byte array to decode", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Text", "Txt", "Decoded text", GH_ParamAccess.item);
+        }
+
+        #endregion
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            BytesGoo bytes = null;
+
+            if (!DA.GetData(0, ref bytes)) return;
+
+            string txt = System.Text.Encoding.UTF8.GetString(bytes.Value);
+
+            DA.SetData(0, txt);
+        }
+    }
+}
---
+++ Portal.Gh/Components/Utils/BytesEncoderComponent.cs
@@ -0,0 +1,56 @@
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Portal.Gh.Common;
+using Portal.Gh.Params.Bytes;
+
+namespace Portal.Gh.Components.Utils
+{
+    public class BytesEncoderComponent : GH_Component
+    {
+        #region Metadata
+
+        public BytesEncoderComponent()
+            : base("Bytes Encoder", "EnBytes",
+                "Encode string into bytes array",
+                Config.Category, Config.SubCat.Utils)
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+        public override IEnumerable<string> Keywords => new string[] { };
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("b32f547d-0bd2-464a-9765-2fa4af720891");
+
+        #endregion
+
+        #region IO
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Text", "Txt", "Text to serialize", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddParameter(new BytesParam(), "Bytes", "Bytes", "Bytes array of the text", GH_ParamAccess.item);
+        }
+
+        #endregion
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string txt = null;
+
+            if (!DA.GetData(0, ref txt)) return;
+
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(txt);
+            BytesGoo bytesGoo = new BytesGoo(bytes);
+
+            DA.SetData(0, bytesGoo);
+        }
+    }
+}
