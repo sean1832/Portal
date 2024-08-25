f0e83a202037bb191010119cd76017b53b490df5
Zeke Zhang <sean1832725142@gmail.com>
2024-08-25 11:15:30 +1000

    chore(mmf): add keyword for searchability

--- Portal.Gh/Components/Local/SharedMemoryWriterComponent.cs
+++ Portal.Gh/Components/Local/SharedMemoryWriterComponent.cs
@@ -34,7 +34,7 @@ namespace Portal.Gh.Components.Local
         }
 
         public override GH_Exposure Exposure => GH_Exposure.secondary;
-        public override IEnumerable<string> Keywords => new string[] { "memory write", "mmf" };
+        public override IEnumerable<string> Keywords => new string[] { "memory write", "mmfwriter", "mmf writer" };
         protected override Bitmap Icon => Icons.SharedMemoryWriter;
         public override Guid ComponentGuid => new Guid("e44d5593-6665-4396-907f-751c8bc72e03");
 
