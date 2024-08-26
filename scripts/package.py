c65e945353037ac66480352866bf4bacd09f5f70
Zeke Zhang <sean1832725142@gmail.com>
2024-08-26 21:14:08 +1000

    build): update build automation logic for streamlined packaging workflow

--- scripts/package.py
+++ scripts/package.py
@@ -1,13 +1,24 @@
 import argparse
 import os
+import subprocess
 
+import yaml
 from vspacker import package_builder as builder
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Package files to zip")
     parser.add_argument("project_file", help="project file")
+    parser.add_argument("--runyak", action="store_true", help="runyak")
     return parser.parse_args()
+
+
+def update_manifest_version(manifest_file, version):
+    with open(manifest_file, "r") as f:
+        manifest = yaml.safe_load(f)
+    manifest["version"] = version
+    with open(manifest_file, "w") as f:
+        yaml.dump(manifest, f)
 
 
 def main():
@@ -16,13 +27,43 @@ def main():
     solution_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     example_file = os.path.join(solution_root, "Example/grasshopper", "portal-example.gh")
 
-    bd = builder.AssemblyBuilder(project_file)
-    bd.build_zip(
-        zip_filename=f"{bd.project_name}_v{bd.version}.zip",
-        exclude_patterns=["RhinoCommon.dll", "Grasshopper.dll", "*.xml", "*.pdb"],
-        include_files=["LICENSE", "README.md"],
+    proj = builder.AssemblyBuilder(project_file)
+    proj.build_zip(
+        input_dir=os.path.join(proj.project_dir, "bin", "release", "net48"),
+        zip_filename=f"{proj.project_name}-{proj.version}.zip",
+        exclude_patterns=["*.xml", "*.pdb"],
+        include_files=[
+            os.path.join(solution_root, "LICENSE"),
+            os.path.join(solution_root, "README.md"),
+        ],
+        internal_folder=proj.project_name,
     )
-    bd.package_example(example_file)
+    proj.build_folder(
+        dir_paths=os.path.join(proj.project_dir, "bin", "release", "net48"),
+        folder_name="yak",
+        exclude_patterns=["*.xml", "*.pdb"],
+        include_files=[
+            os.path.join(solution_root, "LICENSE"),
+            os.path.join(solution_root, "README.md"),
+            os.path.join(solution_root, "manifest.yml"),
+            (os.path.join(solution_root, "Design", "Logo@512x-80.jpg"), "icon.jpg"),
+        ],
+    )
+    update_manifest_version(
+        os.path.join(proj.output_folder, "yak", "manifest.yml"),
+        proj.version,
+    )
+    proj.copy_file(
+        example_file,
+        proj.output_folder,
+        rename=f"{proj.project_name}-example-{proj.version}.gh",
+    )
+
+    if args.runyak:
+        # go to the output folder
+        os.chdir(os.path.join(proj.output_folder, "yak"))
+        # call yak command
+        subprocess.run(["yak", "build", "--platform", "win"])
 
 
 if __name__ == "__main__":
