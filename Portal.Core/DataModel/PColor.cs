f82cd56dbab320abb68019d9f86f0739e975bc58
Zeke Zhang <sean1832725142@gmail.com>
2024-08-29 16:54:39 +1000

    feat(srMesh): add vertex color property for mesh datamodel for de / serialization
    - remove unnessary mesh properties
      - `uvs`
      - `normals`

---
+++ Portal.Core/DataModel/PColor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Core.DataModel
+{
+    public class PColor
+    {
+        public Byte R { get;}
+        public Byte G { get;}
+        public Byte B { get;}
+        public Byte A { get;}
+
+        public PColor() { }
+
+        public PColor(Byte r, Byte g, Byte b, Byte a)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+
+        public PColor(Byte r, Byte g, Byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = 255;
+        }
+
+        public PColor(byte[] array)
+        {
+            if (array.Length != 4)
+            {
+                throw new ArgumentException("Array must have 4 elements");
+            }
+
+            R = array[0];
+            G = array[1];
+            B = array[2];
+            A = array[3];
+        }
+
+        public string ToHex()
+        {
+            return $"#{R:X2}{G:X2}{B:X2}{A:X2}";
+        }
+
+        public byte[] ToByteArray() {
+            return new byte[] { R, G, B, A };
+        }
+
+        public override string ToString()
+        {
+            return $"R:{R}, G:{G}, B:{B}, A:{A}";
+        }
+
+        public override int GetHashCode() {
+            return R.GetHashCode() ^ G.GetHashCode() ^ B.GetHashCode() ^ A.GetHashCode();
+        }
+
+        public static PColor FromHexColor(string hexColor)
+        {
+            if (hexColor.Length != 7 && hexColor.Length != 9)
+            {
+                throw new ArgumentException("Hex color must be 7 or 9 characters long");
+            }
+
+            if (hexColor[0] != '#')
+            {
+                throw new ArgumentException("Hex color must start with #");
+            }
+
+            byte r = Convert.ToByte(hexColor.Substring(1, 2), 16);
+            byte g = Convert.ToByte(hexColor.Substring(3, 2), 16);
+            byte b = Convert.ToByte(hexColor.Substring(5, 2), 16);
+            byte a = hexColor.Length == 9 ? Convert.ToByte(hexColor.Substring(7, 2), 16) : (byte)255;
+
+            return new PColor(r, g, b, a);
+        }
+    }
+}
