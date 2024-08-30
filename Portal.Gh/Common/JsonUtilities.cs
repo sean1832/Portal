958cf8fc3ecdbf370d95b417c574b20c6c8c774a
Zeke Zhang <sean1832725142@gmail.com>
2024-08-31 04:37:09 +1000

    feat(gh): implement custom JSON Dictionary Grasshopper data type

---
+++ Portal.Gh/Common/JsonUtilities.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using Portal.Gh.Params.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Gh.Common
+{
+    public static class JsonUtilities
+    {
+        public static object ToSimpleValue(this JToken token)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Integer:
+                    return token.Value<long>();
+                case JTokenType.String:
+                    return token.Value<string>();
+                case JTokenType.TimeSpan:
+                    return token.Value<TimeSpan>();
+                case JTokenType.Boolean:
+                    return token.Value<bool>();
+                case JTokenType.Date:
+                    return token.Value<DateTime>();
+                case JTokenType.Float:
+                    return token.Value<double>();
+                default:
+                    return new JTokenGoo(token);
+            }
+        }
+    }
+}
---
+++ Portal.Gh/Params/Json/JsonDictParam.cs
@@ -0,0 +1,69 @@
+﻿using Grasshopper.Kernel;
+using Portal.Gh.Common;
+using Portal.Gh.Params.Bytes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using GH_IO.Serialization;
+
+namespace Portal.Gh.Params.Json
+{
+    public class JsonDictParam : GH_PersistentParam<JsonDictGoo>, IDisposable
+    {
+        public JsonDictParam() :
+            base("Json", "Json",
+                "Represents a Json Object",
+                Config.Category, Config.SubCat.Params)
+        {
+        }
+        protected override Bitmap Icon => null;
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
+        public override Guid ComponentGuid => new Guid("c5726694-d8eb-4eb4-aa55-ded1ca9da417");
+
+        protected override GH_GetterResult Prompt_Singular(ref JsonDictGoo value)
+        {
+            return GH_GetterResult.cancel;
+        }
+
+        protected override GH_GetterResult Prompt_Plural(ref List<JsonDictGoo> values)
+        {
+            return GH_GetterResult.cancel;
+        }
+
+        public override bool Write(GH_IWriter writer)
+        {
+            var result = base.Write(writer);
+            writer.SetInt32("ScriptParamAccess", (int)Access);
+            return result;
+        }
+
+
+        public override bool Read(GH_IReader reader)
+        {
+            var result = base.Read(reader);
+
+            if (reader.ItemExists("ScriptParamAccess"))
+            {
+                try
+                {
+                    Access = (GH_ParamAccess)reader.GetInt32("ScriptParamAccess");
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    //smoosh
+                }
+            }
+            return result;
+        }
+
+        public void Dispose()
+        {
+            this.ClearData();
+        }
+    }
+}
