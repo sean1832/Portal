30cd9416c7b49236e465e0ae1efd297570928e6f
Zeke Zhang <sean1832725142@gmail.com>
2024-08-31 04:34:52 +1000

    feat(core): add JSON payload structure with data and metadata fields

---
+++ Portal.Core/DataModel/JsonDict.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Portal.Core.DataModel
+{
+    public class JsonDict : Dictionary<string, object>, IDisposable
+    {
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+        public void Dispose()
+        {
+            Clear();
+        }
+    }
+}
