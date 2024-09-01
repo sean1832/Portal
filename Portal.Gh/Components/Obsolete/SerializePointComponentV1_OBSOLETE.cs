e1b36f8d12159b0b5cedb2a3c21f88b740c0d024
Zeke Zhang <sean1832725142@gmail.com>
2024-09-01 22:56:47 +1000

    feat(gh-serialization): serialize metadata at serialization components
    Moved metadata serialization from the pack payload component to serialization components for better logical separation.

--- Portal.Gh/Components/Obsolete/SerializePointComponentV1_OBSOLETE.cs
+++ Portal.Gh/Components/Obsolete/SerializePointComponentV1_OBSOLETE.cs
@@ -1,31 +1,39 @@
+using Grasshopper;
 using Grasshopper.Kernel;
 using Portal.Gh.Common;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using GH_IO.Serialization;
 using Newtonsoft.Json;
 using Portal.Core.DataModel;
 using Portal.Gh.Params.Json;
-using Portal.Gh.Params.Payloads;
+using Point = Rhino.Geometry.Point;
+using Rhino.Render.DataSources;
 
-namespace Portal.Gh.Components.Serialization
+namespace Portal.Gh.Components.Obsolete
 {
-    public class SerializePointComponent : GH_Component
+    public class SerializePointComponentV1_OBSOLETE : GH_Component
     {
         #region Metadata
 
-        public SerializePointComponent()
+        public SerializePointComponentV1_OBSOLETE()
             : base("Serialize Points", "SrPt",
                 "Serialize points into a JSON representation. This data should be pack as payload using `Pack Payload` before send over communication pipeline for data exchange.",
                 Config.Category, Config.SubCat.Serialization)
         {
         }
 
-        public override GH_Exposure Exposure => GH_Exposure.primary;
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
         public override IEnumerable<string> Keywords => new string[] { };
         protected override Bitmap Icon => Icons.SerializePoint;
-        public override Guid ComponentGuid => new Guid("3c9bdd0a-ae6b-49ed-b95d-e1f98c3e5eeb");
+        public override Guid ComponentGuid => new Guid("cacea36d-2987-4b63-8975-8d0f886e1454");
 
         #endregion
 
@@ -34,15 +42,11 @@ namespace Portal.Gh.Components.Serialization
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddPointParameter("Points", "Pts", "Points to serialize", GH_ParamAccess.item);
-            pManager.AddParameter(new JsonDictParam(), "Metadata", "#", "(Optional) Metadata that describe the geometry",
-                GH_ParamAccess.item);
-
-            pManager[1].Optional = true;
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddParameter(new PayloadParam(), "Payload", "P", "Structured packet payload", GH_ParamAccess.item);
+            pManager.AddParameter(new JsonDictParam(), "Json", "Json", "Serialized point as JSON object", GH_ParamAccess.item);
         }
 
         #endregion
@@ -51,19 +55,16 @@ namespace Portal.Gh.Components.Serialization
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             Point3d data = new Point3d();
-            JsonDictGoo metaGoo = new JsonDictGoo();
 
             if (!DA.GetData(0, ref data)) return;
-            DA.GetData(1, ref metaGoo);
 
-            DA.SetData(0, SerializePoint(data, metaGoo.Value));
+            DA.SetData(0, SerializePoint(data));
         }
-        private PayloadGoo SerializePoint(Point3d point, JsonDict meta)
+        private JsonDictGoo SerializePoint(Point3d point)
         {
             string jsonString = JsonConvert.SerializeObject(new PVector3D(point.X, point.Y, point.Z));
             JsonDict dict = JsonConvert.DeserializeObject<JsonDict>(jsonString);
-
-            return new PayloadGoo(new Payload(dict, meta));
+            return new JsonDictGoo(dict);
         }
     }
 }
