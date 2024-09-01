0dd780add25aa410e82650ddb38550f495b39bbd
Zeke Zhang <sean1832725142@gmail.com>
2024-09-01 22:56:47 +1000

    feat(gh-serialization): serialize metadata at serialization components
    Moved metadata serialization from the pack payload component to serialization components for better logical separation.

--- Portal.Gh/Components/Obsolete/SerializeMeshComponentV1_OBSOLETE.cs
+++ Portal.Gh/Components/Obsolete/SerializeMeshComponentV1_OBSOLETE.cs
@@ -9,26 +9,25 @@ using Newtonsoft.Json;
 using Portal.Core.DataModel;
 using Portal.Gh.Common;
 using Portal.Gh.Params.Json;
-using Portal.Gh.Params.Payloads;
 using Rhino.Geometry.Collections;
 
-namespace Portal.Gh.Components.Serialization
+namespace Portal.Gh.Components.Obsolete
 {
-    public class SerializeMeshComponent : GH_Component
+    public class SerializeMeshComponentV1_OBSOLETE : GH_Component
     {
         #region Metadata
 
-        public SerializeMeshComponent()
+        public SerializeMeshComponentV1_OBSOLETE()
             : base("Serialize Mesh", "SrMesh",
-                "Serialize meshes into a payload. This payload should be pack using `Pack Payload` before send over communication pipeline for data exchange.",
+                "Serialize meshes into a JSON representation. This data should be pack as payload using `Pack Payload` before send over communication pipeline for data exchange.",
                 Config.Category, Config.SubCat.Serialization)
         {
         }
 
-        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
         public override IEnumerable<string> Keywords => new string[] { };
         protected override Bitmap Icon => Icons.SerializeMesh;
-        public override Guid ComponentGuid => new Guid("a6d957ac-89ae-420d-ad0f-3c0fa516251f");
+        public override Guid ComponentGuid => new Guid("9b042bd6-2272-48b4-afbc-ddb155624e0f");
 
         #endregion
 
@@ -37,15 +36,11 @@ namespace Portal.Gh.Components.Serialization
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddMeshParameter("Mesh", "M", "Mesh to serialize", GH_ParamAccess.item);
-            pManager.AddParameter(new JsonDictParam(), "Metadata", "#", "(Optional) Metadata that describe the geometry",
-                GH_ParamAccess.item);
-
-            pManager[1].Optional = true;
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddParameter(new PayloadParam(), "Payload", "P", "Structured packet payload", GH_ParamAccess.item);
+            pManager.AddParameter(new JsonDictParam(), "Json", "Json", "Serialized mesh as JSON object", GH_ParamAccess.item);
         }
 
         #endregion
@@ -53,15 +48,10 @@ namespace Portal.Gh.Components.Serialization
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             Mesh mesh = new Mesh();
-            JsonDictGoo metaGoo = new JsonDictGoo();
 
             if (!DA.GetData(0, ref mesh)) return;
-            DA.GetData(1, ref metaGoo);
 
-            JsonDictGoo meshGoo = SerializeMesh(mesh);
-            PayloadGoo payloadGoo = new PayloadGoo(new Payload(meshGoo.Value, metaGoo.Value));
-
-            DA.SetData(0, payloadGoo);
+            DA.SetData(0, SerializeMesh(mesh));
         }
 
         private JsonDictGoo SerializeMesh(Mesh mesh)
