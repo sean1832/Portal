1aa1f7c01180f2318aaf3ffb8caf19a92d388797
Zeke Zhang <sean1832725142@gmail.com>
2024-09-01 22:52:06 +1000

    feat(portal-datamodel): add payload constructor & ToString override

--- Portal.Core/DataModel/Payload.cs
+++ Portal.Core/DataModel/Payload.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Portal.Core.DataModel
 {
@@ -10,5 +11,22 @@ namespace Portal.Core.DataModel
     {
         public object Data { get; set; }
         public JsonDict Metadata { get; set; }
+
+        public Payload() { }
+
+        public Payload(object data)
+        {
+            Data = data;
+        }
+        public Payload(object data, JsonDict metadata)
+        {
+            Data = data;
+            Metadata = metadata;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
     }
 }
