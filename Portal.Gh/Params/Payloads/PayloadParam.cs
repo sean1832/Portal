cc0eff528995ae1c7a3e646a472a2cbc1d945130
Zeke Zhang <sean1832725142@gmail.com>
2024-09-01 23:33:30 +1000

    style(payload): add icon for payload param

--- Portal.Gh/Params/Payloads/PayloadParam.cs
+++ Portal.Gh/Params/Payloads/PayloadParam.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eto.Drawing;
 using Portal.Gh.Common;
+using Bitmap = System.Drawing.Bitmap;
 
 namespace Portal.Gh.Params.Payloads
 {
@@ -17,6 +19,7 @@ namespace Portal.Gh.Params.Payloads
         {
         }
         public override GH_Exposure Exposure => GH_Exposure.hidden;
+        protected override Bitmap Icon => Icons.PayloadParam;
         public override Guid ComponentGuid => new Guid("1c9a0445-cb2b-43ac-b6fa-971c2c0b4c0a");
         protected override GH_GetterResult Prompt_Singular(ref PayloadGoo value)
         {
